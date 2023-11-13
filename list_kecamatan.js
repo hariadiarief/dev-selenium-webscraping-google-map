f5bfa4b44c84e020142fe503715cf9fa47a12f86
hariadiarief <adi.arief77@gmail.com>
2023-11-13 14:42:42 +0700

    first commit

---
+++ list_kecamatan.js
@@ -0,0 +1,303 @@
+list_coordinates_kecamatan = [
+    {
+        area: 'Bekasi',
+        coordonates: [
+            {
+                "kota": "Bekasi",
+                "kecamatan": "Bantar Gebang",
+                "lat": "-6.339641",
+                "long": "106.988990"
+            },
+            {
+                "kota": "Bekasi",
+                "kecamatan": "Bekasi Barat",
+                "lat": "-6.227953",
+                "long": "106.960425"
+            },
+            {
+                "kota": "Bekasi",
+                "kecamatan": "Bekasi Selatan",
+                "lat": "-6.258244",
+                "long": "106.977183"
+            },
+            {
+                "kota": "Bekasi",
+                "kecamatan": "Bekasi Timur",
+                "lat": "-6.236254",
+                "long": "107.024417"
+            },
+            {
+                "kota": "Bekasi",
+                "kecamatan": "Bekasi Utara",
+                "lat": "-6.206405",
+                "long": "107.000799"
+            },
+            {
+                "kota": "Bekasi",
+                "kecamatan": "Jatiasih",
+                "lat": "-6.339641",
+                "long": "106.988990"
+            },
+            {
+                "kota": "Bekasi",
+                "kecamatan": "Jatisampurna",
+                "lat": "-6.335609",
+                "long": "106.923506"
+            },
+            {
+                "kota": "Bekasi",
+                "kecamatan": "Medan Satria",
+                "lat": "-6.194132",
+                "long": "106.986038"
+            },
+            {
+                "kota": "Bekasi",
+                "kecamatan": "Mustika Jaya",
+                "lat": "-6.302860",
+                "long": "107.018512"
+            },
+            {
+                "kota": "Bekasi",
+                "kecamatan": "Pondok Gede",
+                "lat": "-6.270025",
+                "long": "106.929958"
+            },
+            {
+                "kota": "Bekasi",
+                "kecamatan": "Pondok Melati",
+                "lat": "6.3068329",
+                "long": "106.9331368"
+            },
+            {
+                "kota": "Bekasi",
+                "kecamatan": "Rawalumbu",
+                "lat": "-6.2778973",
+                "long": "107.0007987"
+            }
+        ]
+    },
+    {
+        area: 'Jakarta Timur',
+        coordonates: [
+            {
+                "kota": "Jakarta Timur",
+                "kecamatan": "Pasar Rebo",
+                "lat": "-6.326164",
+                "long": "106.856188"
+            },
+            {
+                "kota": "Jakarta Timur",
+                "kecamatan": "Ciracas",
+                "lat": "-6.323116",
+                "long": "106.870940"
+            },
+            {
+                "kota": "Jakarta Timur",
+                "kecamatan": "Cipayung",
+                "lat": "-6.327251",
+                "long": "106.900447"
+            },
+            {
+                "kota": "Jakarta Timur",
+                "kecamatan": "Makasar",
+                "lat": "-6.271194",
+                "long": "106.894547"
+            },
+            {
+                "kota": "Jakarta Timur",
+                "kecamatan": "Kramatjati",
+                "lat": "-6.273298",
+                "long": "106.869465"
+            },
+            {
+                "kota": "Jakarta Timur",
+                "kecamatan": "Jatinegara",
+                "lat": "-6.2284",
+                "long": "106.8852"
+            },
+            {
+                "kota": "Jakarta Timur",
+                "kecamatan": "Sawit",
+                "lat": "-6.229541",
+                "long": "106.918153"
+            },
+            {
+                "kota": "Jakarta Timur",
+                "kecamatan": "Cakung",
+                "lat": "-6.182629",
+                "long": "106.947666"
+            },
+            {
+                "kota": "Jakarta Timur",
+                "kecamatan": "Pulogadung",
+                "lat": "-6.189464",
+                "long": "106.894546"
+            },
+            {
+                "kota": "Jakarta Timur",
+                "kecamatan": "Matraman",
+                "lat": "-6.203285",
+                "long": "106.862089"
+            },
+        ]
+    },
+    {
+        area: 'Jakarta Selatan',
+        coordonates: [
+            {
+                "kota": "Jakarta Selatan",
+                "kecamatan": "Cilandak",
+                "lat": "-6.284528",
+                "long": "106.800140",
+            },
+            {
+                "kota": "Jakarta Selatan",
+                "kecamatan": "Jagakarsa",
+                "lat": "-6.334917",
+                "long": "106.823737",
+            },
+            {
+                "kota": "Jakarta Selatan",
+                "kecamatan": "Kebayoran Baru",
+                "lat": "-6.243622",
+                "long": "106.800140",
+            },
+            {
+                "kota": "Jakarta Selatan",
+                "kecamatan": "Kebayoran Lama",
+                "lat": "-6.244392",
+                "long": "106.776544",
+            },
+            {
+                "kota": "Jakarta Selatan",
+                "kecamatan": "Mampang Prapatan",
+                "lat": "-6.250614",
+                "long": "106.820788",
+            },
+            {
+                "kota": "Jakarta Selatan",
+                "kecamatan": "Pancoran",
+                "lat": "-6.252300",
+                "long": "106.847338",
+            },
+            {
+                "kota": "Jakarta Selatan",
+                "kecamatan": "Pasar Minggu",
+                "lat": "-6.293981",
+                "long": "106.823737",
+            },
+            {
+                "kota": "Jakarta Selatan",
+                "kecamatan": "Pesanggrahan",
+                "lat": "-6.247428",
+                "long": "106.761798",
+            },
+            {
+                "kota": "Jakarta Selatan",
+                "kecamatan": "Setiabudi",
+                "lat": "-6.219569",
+                "long": "106.832587",
+            },
+            {
+                "kota": "Jakarta Selatan",
+                "kecamatan": "Tebet",
+                "lat": "-6.231860",
+                "long": "106.847338",
+            },
+        ]
+    },
+    {
+        area: 'Jakarta Barat',
+        coordonates: [
+            {
+                "kota": "Jakarta Barat",
+                "kecamatan": "Cengkareng",
+                "lat": "-6.152671",
+                "long": "106.740384",
+            },
+            {
+                "kota": "Jakarta Barat",
+                "kecamatan": "Grogol Petamburan",
+                "lat": "-6.160856",
+                "long": "106.792766",
+            },
+            {
+                "kota": "Jakarta Barat",
+                "kecamatan": "Taman Sari",
+                "lat": "-6.639925",
+                "long": "106.758849",
+            },
+            {
+                "kota": "Jakarta Barat",
+                "kecamatan": "Tambora",
+                "lat": "-6.143655",
+                "long": "106.791376",
+            },
+            {
+                "kota": "Jakarta Barat",
+                "kecamatan": "Kebon Jeruk",
+                "lat": "-6.195942",
+                "long": "106.773595",
+            },
+            {
+                "kota": "Jakarta Barat",
+                "kecamatan": "Kalideres",
+                "lat": "-6.134294",
+                "long": "106.705773",
+            },
+            {
+                "kota": "Jakarta Barat",
+                "kecamatan": "Palmerah",
+                "lat": "-6.190071",
+                "long": "106.797190",
+            },
+            {
+                "kota": "Jakarta Barat",
+                "kecamatan": "Kembangan",
+                "lat": "1.320982",
+                "long": "103.912842",
+            },
+        ]
+    },
+    {
+        area: 'Jakarta Utara',
+        coordonates: [
+            {
+                "kota": "Jakarta Utara",
+                "kecamatan": "Cilincing",
+                "lat": "-6.1102",
+                "long": "106.9449",
+            },
+            {
+                "kota": "Jakarta Utara",
+                "kecamatan": "Kelapa Gading",
+                "lat": "-6.1613",
+                "long": "106.9050",
+            },
+            {
+                "kota": "Jakarta Utara",
+                "kecamatan": "Koja",
+                "lat": "-6.1159",
+                "long": "106.9081",
+            },
+            {
+                "kota": "Jakarta Utara",
+                "kecamatan": "Pademangan",
+                "lat": "-6.1339",
+                "long": "106.8384",
+            },
+            {
+                "kota": "Jakarta Utara",
+                "kecamatan": "Penjaringan",
+                "lat": "-6.1165",
+                "long": "106.7558",
+            },
+            {
+                "kota": "Jakarta Utara",
+                "kecamatan": "Tanjung Priok",
+                "lat": "-6.1383",
+                "long": "106.8665",
+            }
+        ]
+    }
+]
